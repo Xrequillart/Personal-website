76bd4ecbd68daf7cbecb95f0b60c1db37fba273f
Xavier Réquillart <xrequillart@gmail.com>
2018-03-25 13:57:44 +0200

    update color

--- moz/theme.js
+++ moz/theme.js
@@ -5,6 +5,14 @@ export default {
   red: {
     light: '#FF5959',
   },
+  purple: {
+    light: '#7E63D2',
+    normal: '#493097',
+    dark: '#000A49',
+  },
+  green: {
+    light: '#50E3C2',
+  },
   white: '#FFFFFF',
   black: '#000000',
 };
