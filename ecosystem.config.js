6420638af17434a4cc70f690b3e4e3c456aabd4e
Xavier Réquillart <xrequillart@gmail.com>
2018-03-28 22:34:54 +0200

    direct push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,9 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 0,
       env: {
-        PORT: '3000',
         NODE_ENV: 'development',
       },
       env_production: {
-        PORT: '8080',
         NODE_ENV: 'production',
       },
     },
