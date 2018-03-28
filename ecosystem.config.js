cdc9e89cfa9a7af709f48f13b8d7087d65ec0c01
Xavier Réquillart <xrequillart@gmail.com>
2018-03-28 21:44:25 +0200

    update env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,9 +6,11 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 0,
       env: {
+        "PORT": 3000,
         "NODE_ENV": "development"
       },
       env_production: {
+        "PORT": 8080,
         "NODE_ENV": "production",
       }
     },
