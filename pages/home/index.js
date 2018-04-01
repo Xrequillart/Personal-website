a365490d83256c4de18d3c19b55bc6ac0cf9646f
Xavier Réquillart <xrequillart@gmail.com>
2018-04-01 18:23:13 +0200

    cleaning code

--- pages/home/index.js
+++ pages/home/index.js
@@ -9,30 +9,33 @@ import Container from '../../components/container';
 
 import styles from './styles';
 
+const links = [
+  { href: 'https://www.linkedin.com/in/xrequillart/', kind: 'primary', name: 'LinkedIn' },
+  { href: 'https://github.com/Xrequillart', kind: 'success', name: 'Github' }
+];
+
 const DefaultPage = (props) => {
   const { className } = props;
   return (
     $('div', null,
       $(Head, null,
         $('title', null, 'Xavier Réquillart Full stack developer sur Paris'),
-        $('meta', { name: 'description', content: "Hello, je suis Xavier Réquillart. Full stack developer javascript. Jai utilise ReactJS et NextJS pour réaliser mon site personnel."}),
+        $('meta', { name: 'description', content: "Hello, je suis Xavier Réquillart. Full stack developer javascript. Jai utilise ReactJS et NextJS pour réaliser mon site personnel." }),
       ),
       $(Container, null,
-        $(Title, null, 
+        $(Title, null,
           'Hello,',
           $('br'),
           "i'm Xavier Réquillart",
           $('br'),
           'Full stack developer',
         ),
-        $('div', { className }, 
-          $('a', { href: 'https://www.linkedin.com/in/xrequillart/', className: 'item' }, $(Button, { kind: 'primary' }, 'linkedIn')),
-          $('a', { href: 'https://github.com/Xrequillart', className: 'item' }, $(Button, { kind: 'success' }, 'Github')),
-          $('a', { href: 'https://dribbble.com/xrequillart', className: 'item' }, $(Button, { kind: 'secondary' }, 'Dribble')),
+        $('div', { className },
+          links.map((link) => $('a', { href: link.href, className: 'item' }, $(Button, { kind: link.kind }, link.name))),
         ),
       ),
     )
   )
 }
 
-export default styled(DefaultPage)`${styles}`;
+export default styled(DefaultPage) `${styles}`;
