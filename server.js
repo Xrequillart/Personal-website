16adfc6565c6ef494a15f3233518064d6af3a3c9
Xavier Réquillart <xrequillart@gmail.com>
2018-11-16 11:42:05 +0100

    fix port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
+const PORT = 3000;
+
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
@@ -11,8 +13,8 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url, true);
     const { pathname, query } = parsedUrl;
     handle(req, res, parsedUrl);
-  }).listen(3000, err => {
+  }).listen(PORT, err => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000');
+    console.log(`> Ready on http://localhost:${PORT}`);
   })
 })
